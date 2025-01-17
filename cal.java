e21a32399b285f4f6d875652910e7e6febb396b9
Krishna gopal singh <krishnagopalsingh587@gmail.com>
2025-01-17 16:34:34 +0530

    sdsfsd

--- cal.java
+++ cal.java
@@ -3,8 +3,8 @@ public class cal{
         System.out.println("hey");
     }
     public static int addition (int val, int val2){
-        int value = 0;
-        return val+val2;
+        int value = val;
+        return value+val2;
     }
     //hello there this is divyanshu
     //this is the best working of world
